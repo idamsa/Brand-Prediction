70c32d698f8b2034630e296c8b5d3045c5e44a14
Damsa Ioana <damsaioana17@gmail.com>
2019-11-28 09:48:52 +0100

    Final R script brand prediction

--- R Task Pred Brand Script.R
+++ R Task Pred Brand Script.R
@@ -1,3 +1,11 @@
+##################################################################
+# Task 2 Part 2 of Course
+# Predict which Brand of Products Customers Prefer
+##################################################################
+
+
+#loading the libraies needed
+
 library(caret)
 library(ggplot2)
 library(readr)
@@ -5,31 +13,41 @@ library(outliers)
 library(OneR)
 library(Hmisc)
 library(C50)
+library(rattle)
+library(rpart)
 
-#bringing the data_____________________________________________________________________________
+# Reading the file
 
-complete<-read.csv("CompleteResponses.csv")
+complete <- read.csv("CompleteResponses.csv")
 
-#about the data______________________________________________________________________________
+# Data summary
 
 summary(complete)
+
+# Data structure
 str(complete)
-nrow(complete)
 
-#normally distributed?
-shapiro.test(complete$salary) ###data is not normally distributed
 
-#PLOTTING THE DATA exploratory________________________________________________________________
+# Shapiro test to check if the data is normally distributed
+shapiro.test(complete$salary) 
+
+####################################################
+# Exploratory plotting of the data
+####################################################
+
+# Histograms on some of the variables checking for the distribution
 
 hist(complete$credit, freq = F)
 hist(complete$age, freq = F) 
 hist(complete$salary, freq = FALSE)
 
+# Histogram of the salary with the distribution line superpossed
+
 hist(complete$salary,probability=T, main="data distrbution
 ",xlab="salary")
 lines(density(complete$salary),col=2)
 
-#salary hist
+# Histogram od the salary with ggplot
 
 ggplot(complete,aes(x=salary))+
   geom_histogram(bins = 30,color="black",fill="cyan2")+
@@ -37,81 +55,86 @@ ggplot(complete,aes(x=salary))+
   scale_x_continuous(name="Salary",breaks=seq(20000,500000,50000))
 
 
-#credit hist
+# Histogram od the credit with ggplot
 
 ggplot(complete,aes(x=credit))+
   geom_histogram(binwidth  =20000,color="black",fill="cyan2")+
   ggtitle("The Distribution of the Credit")+
   scale_x_continuous(name="Credit", breaks=seq(0,500000,50000))
 
-#brand only related bar/pie 
+# Barchart of the two brands
 
 ggplot(complete,aes(x=Preferedbrand, fill=Preferedbrand))+
   geom_bar()+
   labs(title="Prefered Brands by Number")+
   theme_light()
 
+# Piechart of the brands
 
-ggplot(complete, aes(x="",fill=Preferedbrand))+
-  geom_bar()+ 
-  coord_polar(theta="y")+
-  labs(x=" ",y=" ", title = "Proportion of Prefered Brands")+
+ggplot(complete, aes(x="", fill=Preferedbrand))+
+  geom_bar() + 
+  coord_polar(theta="y") +
+  labs(x=" ",y=" ", title = "Proportion of Prefered Brands") +
   theme_light()
 
-#Relation age and pref scatter
+# Scatter plot checking the relation between age, salary and brand
 
 ggplot(complete, aes(x=age, y=salary, col=Preferedbrand)) + 
   geom_point()+
   labs(title="Relationship Between Age, Salary and Brand Preference")
 
 
+#######################################################
+#FEATURE SELECTION
+#######################################################
 
-#FEATURE SELECTION ___________________________________________________________________FEATURE SELECTION
+# Running a correlation matrix on the data checking for highly correlated features
 
-#corr matrix, we see car and elevel highly corr we will have to get rid of one of them at least
-#also age and salary low corelated
-res2 <- rcorr(as.matrix(complete))
+print(res2 <- rcorr(as.matrix(complete)))
 
-res2
+# Removing unecessary features
 
-#removing car,elevels,credit attributes
 complete <- complete[c(1,2,7)]
-str(complete)
 
-#_________________________________________________________________________________________
+#######################################################
 #PREPROCESSING
+######################################################
 
-#normalizing the age and salary
+# Normalizing the age and salary
 
 normalize <- function(x) {
-  return ((x - min(x)) / (max(x) - min(x)))
-}
+  return ((x - min(x)) / (max(x) - min(x))) # creates the normalization formula
+} 
  
-complete$salary<-normalize(complete$salary)
-complete$age<-normalize(complete$age)
 
-#transforming from numeric to factors
+complete$salary <- normalize(complete$salary)
+complete$age <- normalize(complete$age)
 
-complete$brand<-as.factor(complete$brand)
+# Transforming from numeric to factors and assigning the levels
+
+complete$brand <- as.factor(complete$brand)
 levels(complete$brand) <- c("Acer","Sony")
-str(complete)
 
 
-#finding missing values
-sum(is.na(complete)) #no missing values
+# Checking for missing values
 
+print(sum(is.na(complete)))
 
-#finding outliers in salary
-outlier_tfSalary = outlier(complete$salary,logical=TRUE)
+# Finding outliers in salary 
+
+outlier_tfSalary <- outlier(complete$salary,logical = TRUE)
 sum(outlier_tfSalary)
 
-#what were the ouliers in salary
-find_outlierSalary = which(outlier_tfSalary==TRUE,arr.ind=TRUE)
-#Removing the outliers in salary
+# Assigning the outliers to a vector
+
+find_outlierSalary <- which(outlier_tfSalary == TRUE, arr.ind = TRUE)
+
+# Removing the outliers in salary
+
 complete= complete[-find_outlierSalary,]
 nrow(complete)
 
-#binning the salary
+# Binning the salary
 
 bin(
   complete$salary,
@@ -120,35 +143,24 @@ bin(
   method = c( "content")
 )
 
-str(complete)
 
-
-
-#_________________________________________________________________________________SPLITTING THE DATA
+####################################################
 #SPLITTING THE DATA
-#creating the trainig and testing sets
+####################################################
+
+# Creating the trainig and testing sets
+
 set.seed(123)
 
-inTrain<- createDataPartition(y= complete$brand,p=.75, list = FALSE) 
+inTrain <- createDataPartition(y= complete$brand,p=.75, list = FALSE) 
 
-str(inTrain)
-
-#partitioning the data
+# Partitioning the data
 
 training <- complete[ inTrain,]
 testing <- complete[-inTrain,]
-nrow(training)
-nrow(testing)
 
 
-
-head(testing)
-head(training)
-str(testing)
-
-#decision tree on the data for feature importance__________________________________________________DECISION TREE ONLY
-
-library(rpart)
+# Decision tree on the data to check how and where it splits
 
 rpartMod = rpart(brand ~ .
                  , data = training
@@ -160,14 +172,15 @@ rpartMod = rpart(brand ~ .
 pred = predict(rpartMod, testing, type = "class")
 sum(testing[, 5] != pred)
 
-#visualize decision tree# we see that the tree chose salary and age as the nodes
+# Plotting the decision tree
 
-library(rattle)
 fancyRpartPlot(rpartMod)
 
-#___________________________________________________________________________________________BUILDING THE MODELS________
-#BUILGING THE MODELS
-#traincontrol-modifies resampling method
+####################################################################
+# BUILDING ,TRAINING AND TESTING THE MODELS
+####################################################################
+
+# Setting the sampling methos Cross Validation
 
 
 ctrl <- trainControl(
@@ -178,9 +191,7 @@ ctrl <- trainControl(
                      classProbs = TRUE
                      )
 
-#____________________________________________________________________________________DECISION TREE
-
-#C5.0 Decision Tree
+# Training the C5.0 Decision Tree
 
 set.seed(123)
 treeFit <- train(
@@ -191,28 +202,16 @@ treeFit <- train(
                  tuneLength = 3,
                 )
 
-treeFit
-varImp(treeFit)
+# Predicting the brand using the Decision Tree
 
-#PREDICT THE BRAND
 treeBrand <- predict(treeFit, newdata = testing)
 
-#computes class probabilities for the model
-treeProbs <- predict(treeFit, newdata = testing, type = "prob")
-treeProbs
-
-#confusion matrix and the statistics
+# Building the confusion matrix 
 
 confusionMatrix(data= treeBrand, testing$brand)
 
-#plot predicted vs observed tree
 
-
-summary(treeFit)
-
-#________________________________________________________________________________________RANDOM FOREST
-
-#Random Forest
+# Training the Random Forest
 
 ctrl <- trainControl(
                      method = "repeatedcv",
@@ -222,14 +221,15 @@ ctrl <- trainControl(
                     )
 
 
-#mtry
+# Setting the mtry
 mtry <- c(2,1,3,5,12,32)
 rfGrid <- expand.grid(.mtry=mtry)
 
 set.seed(123)
 
 
-#train the model
+# Training the Random Forest the model
+
 system.time(
           randomForestFit <- train(
                                    brand~.,
@@ -242,64 +242,61 @@ system.time(
                                   )
             )
 
-randomForestFit
-varImp(randomForestFit)
 
-#PREDICT THE BRAND
+# Predicting the brand using the Random Forest
+
 rfBrand <- predict(randomForestFit, newdata = testing)
 
-#computes class probabilities for the model
+# Compute class probabilities for the model
+
 rfProbs <- predict(randomForestFit, newdata = testing, type = "prob")
 rfProbs
 
-#confusion matrix for the random forest model
+# Compute the confusion matrix for the random forest model
+
 confusionMatrix(data= rfBrand, testing$brand)
 
 
-#importance
+##########################################################################
+# COMPARING THE MODELS
+############################################################################
 
-plot(varImp(randomForestFit))
-
-summary(randomForestFit)
-
-
-####_______________________________________________COMPARING THE MODELS
-
+# Resampling 
 resamps <- resamples(list(tree = treeFit, forest = randomForestFit))
+
 summary(resamps)
 
-#visualize the comparation 
+# Visualizations of  the comparation 
+
 xyplot(resamps, what = "BlandAltman")
 
 dotplot(resamps)
 
 bwplot(resamps)
 
-#For each resample, there are paired results a paired t–test can be used to assess whether there
-#is a difference in the average resampled area under the ROC curve
 
 diffs <- diff(resamps)
 summary(diffs)
-plot.train(treeFit)
-#rf better
 
-###APPLYING THE RandomTree MODEL ON THE INCOMPLETE DATA_____________________________________________________
-
+#####################################################################
+# APPLYING THE RandomTree MODEL ON THE INCOMPLETE DATA
+#####################################################################
 
 incompletef <- read.csv("SurveyIncomplete.csv")
 incomplete
 
-#feature sel
+# Feature selection
 
-incomplete<- incompletef[c(1,2,7)]
+incomplete <- incompletef[c(1,2,7)]
 
-#preprocessing
+# Preprocessing
 
-incomplete$salary<-normalize(incomplete$salary)
-incomplete$age<-normalize(incomplete$age)
-incomplete$brand<-as.factor(incomplete$brand)
+incomplete$salary <- normalize(incomplete$salary)
+incomplete$age <- normalize(incomplete$age)
+
+incomplete$brand <- as.factor(incomplete$brand)
 levels(incomplete$brand) <- c("Acer","Sony")
-str(incomplete)
+
 
 bin(
   incomplete$salary,
@@ -308,26 +305,19 @@ bin(
   method = c( "content")
 )
 
-str(incomplete)
 
-#apllying model
+# Apllying the trained model model
 
 finalPredictionBrand <- predict(treeFit, newdata = incomplete, )
 
-finalPredictionBrandProbs <- predict(treeFit, newdata = incomplete, type = "prob" )
-
-finalPredictionBrandProbs
-
-
-#summary predictions
+# Summary predictions
 
 summary(finalPredictionBrand)
 
 
-#construct the csv with the predicted values
+# Construct the csv with the predicted values added 
 
 completeNewData <- incompletef[c(1,2,3,4,5,6)]
 completeNewData <- cbind(completedNewData, predictedBrand = finalPredictionBrand)
-completeNewData
 write.csv(completeNewData, "PredictedBrands.csv")
 
